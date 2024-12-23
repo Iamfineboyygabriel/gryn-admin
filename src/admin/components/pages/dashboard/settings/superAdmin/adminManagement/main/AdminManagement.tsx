a518c85f61495dce1a1c999421f5549648b5ad4d
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2024-12-23 09:45:42 +0100

    fixed the check box for admin

--- src/admin/components/pages/dashboard/settings/superAdmin/adminManagement/main/AdminManagement.tsx
+++ src/admin/components/pages/dashboard/settings/superAdmin/adminManagement/main/AdminManagement.tsx
@@ -7,13 +7,12 @@ import { button } from "../../../../../../../../shared/buttons/Button";
 import plus from "../../../../../../../../assets/svg/plus.svg";
 import CustomPagination from "../../../../../../../../shared/utils/customPagination";
 import { useAllAdminForSuperAdmin } from "../../../../../../../../shared/redux/hooks/admin/getAdminProfile";
-import { deleteUser } from "../../../../../../../../shared/redux/shared/slices/shareApplication.slices";
-import { PrivateElement } from "../../../../../../../../shared/redux/hooks/admin/PrivateElement";
 import { useDispatch } from "react-redux";
+import { deleteUser } from "../../../../../../../../shared/redux/shared/slices/shareApplication.slices";
 
 const SkeletonRow = () => (
   <tr className="animate-pulse border-b border-gray-200">
-    {Array.from({ length: 5 }).map((_, index) => (
+    {Array.from({ length: 6 }).map((_, index) => (
       <td key={index} className="px-6 py-4">
         <div className="h-4 bg-gray-200 rounded"></div>
       </td>
@@ -32,15 +31,17 @@ const AdminManagement = () => {
     fetchAdmins,
     updateSearchTerm,
   } = useAllAdminForSuperAdmin();
-  const dispach = useDispatch();
-  const [selectedAdmins, setSelectedAdmins] = useState<string[]>([]);
+  const dispatch = useDispatch();
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+  const [localSearchTerm, setLocalSearchTerm] = useState(searchTerm || "");
+  const itemsPerPage = 10;
 
-  const handleCheckboxChange = (adminId: string) => {
-    setSelectedAdmins((prev) => {
-      if (prev.includes(adminId)) {
-        return prev.filter((id) => id !== adminId);
+  const handleCheckboxChange = (userId: string) => {
+    setSelectedUsers((prev) => {
+      if (prev.includes(userId)) {
+        return prev.filter((id) => id !== userId);
       } else {
-        return [...prev, adminId];
+        return [...prev, userId];
       }
     });
   };
@@ -50,21 +51,18 @@ const AdminManagement = () => {
       window.confirm("Are you sure you want to delete the selected admins?")
     ) {
       try {
-        const deletePromises = selectedAdmins?.map((adminId) =>
-          dispach(deleteUser(adminId) as any).unwrap()
+        const deletePromises = selectedUsers.map((userId) =>
+          dispatch(deleteUser(userId) as any).unwrap()
         );
 
         await Promise.all(deletePromises);
-        setSelectedAdmins([]);
+        setSelectedUsers([]);
         fetchAdmins(currentPage, itemsPerPage);
       } catch (error) {
         console.error("Error deleting admins:", error);
       }
     }
   };
-
-  const [localSearchTerm, setLocalSearchTerm] = useState(searchTerm || "");
-  const itemsPerPage = 10;
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
@@ -149,28 +147,14 @@ const AdminManagement = () => {
           key={admin?.id}
           className="text-[14px] border-b border-gray-200 leading-[20px] text-grey-primary font-medium"
         >
-          <PrivateElement feature="ALL_STAFFS" page="delete user">
-            <td className="py-[16px] px-[24px]">
-              <div className="relative">
-                <input
-                  type="checkbox"
-                  checked={selectedAdmins.includes(admin?.id)}
-                  onChange={() => handleCheckboxChange(admin?.id)}
-                  className="w-4 h-4 text-primary-700 border-gray-300 rounded focus:ring-primary-700"
-                />
-                <svg
-                  className={`absolute left-0 top-0 w-4 h-4 pointer-events-none
-                    ${selectedAdmins.includes(admin?.id) ? "block" : "hidden"}`}
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill="white"
-                    d="M13.293 4.293a1 1 0 0 1 0 1.414l-6 6a1 1 0 0 1-1.414 0l-3-3a1 1 0 1 1 1.414-1.414L6.586 9.586l5.293-5.293a1 1 0 0 1 1.414 0z"
-                  />
-                </svg>
-              </div>
-            </td>
-          </PrivateElement>
+          <td className="py-[16px] px-[24px]">
+            <input
+              type="checkbox"
+              checked={selectedUsers.includes(admin?.id)}
+              onChange={() => handleCheckboxChange(admin?.id)}
+              className="w-4 h-4 rounded border-gray-300 text-primary-700 focus:ring-primary-700"
+            />
+          </td>
           <td className="py-[16px] px-[24px]">
             {(currentPage - 1) * itemsPerPage + index + 1}
           </td>
@@ -199,7 +183,7 @@ const AdminManagement = () => {
 
     return (
       <tr>
-        <td colSpan={5} className="px-6 py-4 text-center text-gray-500">
+        <td colSpan={6} className="px-6 py-4 text-center text-gray-500">
           <div className="mt-[2em] flex flex-col items-center justify-center">
             <img src={transaction} alt="No admins" />
             <p className="mt-2 text-sm text-gray-500 dark:text-white">
@@ -218,6 +202,8 @@ const AdminManagement = () => {
     highlightText,
     localSearchTerm,
     formatData,
+    selectedUsers,
+    handleCheckboxChange,
   ]);
 
   return (
@@ -226,19 +212,14 @@ const AdminManagement = () => {
         <header className="flex items-center justify-between">
           <h1 className="font-medium text-xl">Admin Management</h1>
           <div className="flex items-center gap-2">
-            <PrivateElement feature="ALL_STAFFS" page="delete user">
-              {selectedAdmins?.length > 0 && (
-                <div>
-                  <button.PrimaryButton
-                    onClick={handleDeleteSelected}
-                    className="mt-[1em] flex gap-2 rounded-full bg-red-500 px-[1.5em] py-[8px] font-medium text-white transition-colors duration-300"
-                  >
-                    Delete Selected ({selectedAdmins.length})
-                  </button.PrimaryButton>
-                </div>
-              )}
-            </PrivateElement>
-
+            {selectedUsers?.length > 0 && (
+              <button.PrimaryButton
+                onClick={handleDeleteSelected}
+                className="mt-[1em] flex gap-2 rounded-full bg-red-500 px-[1.5em] py-[8px] font-medium text-white transition-colors duration-300"
+              >
+                Delete Selected ({selectedUsers.length})
+              </button.PrimaryButton>
+            )}
             <Link to="/admin/dashboard/settings/admin_management/new_admin">
               <button.PrimaryButton className="mt-[1em] flex gap-2 rounded-full bg-primary-700 px-[1.5em] py-[8px] font-medium text-white transition-colors duration-300">
                 <img src={plus} alt="plus" />
@@ -262,11 +243,9 @@ const AdminManagement = () => {
         <table className="w-full mt-4 border-collapse">
           <thead className="text-gray-500 border-b border-gray-200">
             <tr>
-              <PrivateElement feature="ALL_STAFF" page="delete user">
-                <th className="px-6 py-3 text-left text-sm font-normal">
-                  Select
-                </th>
-              </PrivateElement>
+              <th className="px-6 py-3 text-left text-sm font-normal">
+                Select
+              </th>
               <th className="px-6 py-3 text-left text-sm font-normal">S/N</th>
               <th className="px-6 py-3 text-left text-sm font-normal">
                 Full Name
@@ -274,6 +253,9 @@ const AdminManagement = () => {
               <th className="px-6 py-3 text-left text-sm font-normal">Role</th>
               <th className="px-6 py-3 text-left text-sm font-normal">
                 Email Address
+              </th>
+              <th className="px-6 py-3 text-left text-sm font-normal">
+                Action
               </th>
             </tr>
           </thead>
