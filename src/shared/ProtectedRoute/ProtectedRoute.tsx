7b672357033cdba2146d6ef9574139893d387730
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2025-01-02 17:21:24 +0100

    did some fixes

--- src/shared/ProtectedRoute/ProtectedRoute.tsx
+++ src/shared/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,28 @@
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { IdleTimerProvider } from "react-idle-timer";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { logOutUser } from "../redux/shared/slices/shareLanding.slices";
+import { AppDispatch } from "../redux/store";
 
 const ProtectedRoute = () => {
   const navigate = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
 
-  const handleOnIdle = () => {
-    sessionStorage.removeItem("userData");
-    toast.info("You have been logged out due to inactivity.");
+  const handleOnIdle = async () => {
+    const userData = sessionStorage.getItem("userData");
+    if (userData) {
+      const { userId } = JSON.parse(userData);
+      try {
+        await dispatch(logOutUser(userId)).unwrap();
+        toast.info("You have been logged out due to inactivity.");
+      } catch (error) {
+        toast.error("Session expired.");
+      }
+    }
+
+    sessionStorage.clear();
+    localStorage.clear();
     navigate("/");
   };
 
