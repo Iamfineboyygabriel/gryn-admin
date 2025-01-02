6dab8fa7251befdd606426f64c2acf12e6b5ccd1
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2025-01-02 17:47:17 +0100

    fixed the season storage time out

--- src/shared/ProtectedRoute/ProtectedRoute.tsx
+++ src/shared/ProtectedRoute/ProtectedRoute.tsx
@@ -4,31 +4,33 @@ import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { logOutUser } from "../redux/shared/slices/shareLanding.slices";
 import { AppDispatch } from "../redux/store";
+import useUserProfile from "../redux/hooks/shared/getUserProfile";
 
 const ProtectedRoute = () => {
   const navigate = useNavigate();
   const dispatch: AppDispatch = useDispatch();
+  const { userProfile } = useUserProfile();
 
   const handleOnIdle = async () => {
-    const userData = sessionStorage.getItem("userData");
-    if (userData) {
-      const { userId } = JSON.parse(userData);
-      try {
-        await dispatch(logOutUser(userId)).unwrap();
+    try {
+      if (userProfile?.userId) {
+        await dispatch(logOutUser(userProfile.userId)).unwrap();
         toast.info("You have been logged out due to inactivity.");
-      } catch (error) {
-        toast.error("Session expired.");
       }
+      sessionStorage.clear();
+      localStorage.clear();
+      navigate("/");
+    } catch (error) {
+      sessionStorage.clear();
+      localStorage.clear();
+      navigate("/");
+      toast.error("Session expired.");
     }
-
-    sessionStorage.clear();
-    localStorage.clear();
-    navigate("/");
   };
 
   const token = sessionStorage.getItem("userData");
   return token ? (
-    <IdleTimerProvider onIdle={handleOnIdle} timeout={5 * 60 * 1000}>
+    <IdleTimerProvider onIdle={handleOnIdle} timeout={30 * 60 * 1000}>
       <Outlet />
     </IdleTimerProvider>
   ) : (
